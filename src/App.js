892092b0ec6a21e706300b5c72e395a16bc83283
PiratBolot <remedictes@gmail.com>
2019-11-10 19:12:21 +0300

    Добавлена геолокация

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
+import CurrentCity from './components/CurrentCity'
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentPosition: null
+        };
+    }
+
+  updateGeo = (e) => navigator.geolocation.getCurrentPosition(
+      (position) => {
+          this.setState({currentPosition: position})
+      },
+      (e) => {
+          this.setState({error: "Невозможно определить местонахождение"})
+      }
+  );
+
+  componentDidMount = () => {
+      this.updateGeo();
+  };
+
+
   render = () => (
       <div className="App">
-        
+        <div className="App-header">
+          <div className="App-header-text">Погода здесь</div>
+          <button className="App-geo-" onClick={this.updateGeo}>Обновить геолокацию</button>
+        </div>
+          {this.state.currentPosition && <CurrentCity currentPosition={this.state.currentPosition} />}
       </div>
   )
 }
-
 
 export default App;
