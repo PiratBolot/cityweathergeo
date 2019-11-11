417901bd2d2e2162f756a19efcde1a9e03e9b33b
PiratBolot <remedictes@gmail.com>
2019-11-11 18:06:46 +0300

    Добавлен DOM-модель для текущего города

--- src/components/CurrentCity.js
+++ src/components/CurrentCity.js
@@ -1,5 +1,6 @@
 import React from "react";
-import {getWeatherByCoords} from "../ApiHelper";
+import {getWeatherByCoords, parseWeatherResponse} from "../WeatherApi";
+import './CurrentCity.css'
 
 class CurrentCity extends React.Component {
     constructor(props) {
@@ -10,15 +11,45 @@ class CurrentCity extends React.Component {
     async componentDidMount() {
         let data = await getWeatherByCoords(this.state.currentPosition.coords);
         let res = data.response;
-        //let parsedData = parseData(res);
-        this.setState({data: res, city: res.name + ", " + res.sys.country});
+        // console.log(res);
+        // console.log(res.name);
+        let parsedData = parseWeatherResponse(res);
+        this.setState({data: res, parsedWeather: parsedData, city: res.name + ", " + res.sys.country});
+        // console.log(this.state.data.weather[0].icon);
     }
 
 
     render = () => (
-        <div className="CurrentCity">
-
-            <div className="CityName">{this.state.city}</div>
+        <div className="weather_frame">
+            {
+                this.state.data &&
+                <div className="weather_frame_now">
+                    <div className="city_name">{this.state.city}</div>
+                    <div className="date">{new Date().toDateString()}</div>
+                    <div className="weather_now">
+                        <div className="temperature_c">
+                            <span>{(this.state.data.main.temp - 273.14).toFixed(1) + "°C"}</span>
+                        </div>
+                        <img className="weather_icon" src={"http://openweathermap.org/img/wn/" +
+                        this.state.data.weather[0].icon + "@2x.png"} alt=""
+                        />
+                        <div className="weather_description">{this.state.data.weather[0].description}</div>
+                    </div>
+                </div>
+            }
+            {
+                this.state.parsedWeather ?
+                    <div className="wn_body">
+                        {
+                            this.state.parsedWeather.map((e) =>
+                                <div key={e.key} className="wn_line">
+                                    <div className="info_label">{e.key}</div>
+                                    <div className="info_value">{e.value}</div>
+                                </div>
+                            )
+                        }
+                    </div> : ""
+            }
         </div>
     )
 }
