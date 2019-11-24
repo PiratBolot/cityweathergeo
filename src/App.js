73c23dc78e6b4030535151f7c55499b3f41c0885
PiratBolot <remedictes@gmail.com>
2019-11-24 14:07:10 +0300

    Добавлен redux-thunk

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import './App.css';
 import Store from "./components/Store/Store";
 
 import CurrentCity from './components/CurrentCity/CurrentCity'
-import TrackedCity from "./components/TrackedCity/TrackedCity";
 import TrackedCityPanel from "./components/TrackedCityPanel/TrackedCityPanel";
 
 class App extends React.Component {
@@ -19,56 +18,59 @@ class App extends React.Component {
         this.getGeoData = this.getGeoData.bind(this);
     }
 
-  getGeoData = (e) => {
-      if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(
-              (position) => {
-                  Store.dispatch({
-                      type: "updateError",
-                      data: {isError: false, errorMsg: ""}
-                  });
-                  this.setState({currentPosition: position, isGeoAccepted: true, isError: false});
-                  console.log(Store.getState());
-              },
-              (e) => {
-                  Store.dispatch({
-                      type: "updateError",
-                      data: {isError: true, errorMsg: e.message}
-                  });
-                  this.setState({errorMsg: e.message, isError: true,
-                      isGeoAccepted: false});
-                  console.log(Store.getState());
-              },
-              {enableHighAccuracy: false, timeout: 20000, maximumAge: 0}
-          );
-      } else {
-          Store.dispatch({
-              type: "updateError",
-              data: {isError: true, errorMsg: "Геолокация не поддерживается вашим браузером"}
-          });
-          this.setState({errorMsg: "Геолокация не поддерживается вашим браузером", isError: true});
-          console.log(Store.getState());
-      }
+    getGeoData = (e) => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    Store.dispatch({
+                        type: "updateError",
+                        data: {isError: false, errorMsg: ""}
+                    });
+                    this.setState({currentPosition: position, isGeoAccepted: true, isError: false});
+                    console.log(Store.getState());
+                },
+                (e) => {
+                    Store.dispatch({
+                        type: "updateError",
+                        data: {isError: true, errorMsg: e.message}
+                    });
+                    this.setState({
+                        errorMsg: e.message, isError: true,
+                        isGeoAccepted: false
+                    });
+                    console.log(Store.getState());
+                },
+                {enableHighAccuracy: false, timeout: 20000, maximumAge: 0}
+            );
+        } else {
+            Store.dispatch({
+                type: "updateError",
+                data: {isError: true, errorMsg: "Геолокация не поддерживается вашим браузером"}
+            });
+            this.setState({errorMsg: "Геолокация не поддерживается вашим браузером", isError: true});
+            console.log(Store.getState());
+        }
 
-  };
+    };
 
-  componentDidMount = () => {
-      this.getGeoData();
-  };
+    componentDidMount = () => {
+        this.getGeoData();
+    };
 
 
-  render = () => (
-      <div className="App">
-        <div className="app_header">
-          <div className="app_header_text">Погода здесь</div>
-          <button className="app_geo_update_button" onClick={this.getGeoData}>Обновить геолокацию</button>
+    render = () => (
+        <div className="App">
+            <div className="app_header">
+                <div className="app_header_text">Погода здесь</div>
+                <button className="app_geo_update_button" onClick={this.getGeoData}>Обновить геолокацию</button>
+            </div>
+            {this.state.isError && <div className="error_msg">{this.state.errorMsg}</div>}
+            {this.state.currentPosition && <CurrentCity currentPosition={this.state.currentPosition}/>}
+            {!this.state.isGeoAccepted && !this.state.currentPosition &&
+            <CurrentCity currentCity={this.state.reserveCity}/>}
+            <TrackedCityPanel/>
         </div>
-          {this.state.isError && <div className="error_msg">{this.state.errorMsg}</div>}
-          {this.state.currentPosition && <CurrentCity currentPosition={this.state.currentPosition} />}
-          {!this.state.isGeoAccepted && !this.state.currentPosition  && <CurrentCity currentCity={this.state.reserveCity} />}
-          <TrackedCityPanel />
-      </div>
-  )
+    )
 }
 
 export default App;
