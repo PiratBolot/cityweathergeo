a660483c49a564f5978f12580175ca917c79ba8a
PiratBolot <remedictes@gmail.com>
2019-11-27 01:17:36 +0300

    Renamed Action methods

--- src/actions/Actions.js
+++ src/actions/Actions.js
@@ -31,7 +31,7 @@ export function deleteTrackedCity(city) {
     };
 }
 
-function setUpdateError(msg) {
+function setUpdateErrorState(msg) {
     return {
         type: errorTypes.UPDATE_ERROR,
         data: {
@@ -43,11 +43,11 @@ function setUpdateError(msg) {
 
 export function setErrorState(msg) {
     return function (dispatch, getState) {
-        dispatch(setUpdateError(msg));
+        dispatch(setUpdateErrorState(msg));
     };
 }
 
-function resetError() {
+function resetErrorState() {
     return {
         type: errorTypes.NO_ERROR,
         data: {
@@ -57,8 +57,38 @@ function resetError() {
     }
 }
 
-export function resetErrorState() {
+export function resetError() {
     return function (dispatch, getState) {
-        dispatch(resetError());
+        dispatch(resetErrorState());
     };
+}
+
+function setGeolocation(position) {
+    return {
+        type: actionTypes.GET_GEOLOCATION,
+        currentPosition: position
+    }
+}
+
+export function getGeolocation() {
+    return dispatch =>
+        new Promise(
+            (resolve, reject) => {
+                if (navigator.geolocation) {
+                    navigator.geolocation.getCurrentPosition(
+                        (position) => {
+                            dispatch(resetError());
+                            dispatch(setGeolocation(position.coords));
+                        },
+                        (e) => {
+                            dispatch(setErrorState(e.message));
+                            dispatch(setGeolocation({latitude: 61.666668, longitude: 50.816666}));
+                        },
+                        {enableHighAccuracy: false, timeout: 20000, maximumAge: 0}
+                    );
+                } else {
+                    dispatch(setErrorState("Геолокация не поддерживается вашим браузером"));
+                }
+            }
+        )
 }
