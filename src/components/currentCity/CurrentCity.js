6461365d02f2d5ef7bd7db02427640162cfda61c
PiratBolot <remedictes@gmail.com>
2019-11-27 01:18:23 +0300

    Added redux-thunk for CurrentCity

--- src/components/currentCity/CurrentCity.js
+++ src/components/currentCity/CurrentCity.js
@@ -1,33 +1,30 @@
 import React from "react";
-import {getWeatherByCityName, getWeatherByCoords, parseWeatherResponse} from "../../WeatherApi";
+import {getWeatherByCoords, parseWeatherResponse} from "../../WeatherApi";
 import './CurrentCity.css'
 import CircularSpinner from "../preLoader/CircularSpinner";
+import {connect} from "react-redux";
+import {getGeolocation} from "../../actions/Actions";
 
 class CurrentCity extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            currentPosition: props.currentPosition,
-            currentCity: props.currentCity,
             loaded: false
         };
     }
 
-    async componentDidMount() {
-        let data;
-        if (this.state.currentCity) {
-            data = await getWeatherByCityName(this.state.currentCity);
-        } else {
-            data = await getWeatherByCoords(this.state.currentPosition.coords);
+    async componentDidUpdate(prevProps, prevState, snapshot) {
+        if (this.props.geolocation.currentPosition &&
+            this.props.geolocation.currentPosition !== prevProps.geolocation.currentPosition) {
+            let data = await getWeatherByCoords(this.props.geolocation.currentPosition);
+            let res = data.response;
+            let parsedData = parseWeatherResponse(res);
+            this.setState({loaded: !!res, data: res, parsedWeather: parsedData, city: res.name + ", " + res.sys.country});
         }
-        let res = data.response;
-        let parsedData = parseWeatherResponse(res);
+    }
 
-        /* Uncomment if you want to see the preLoader explicitly
-        var answer = prompt('question', 'defaultAnswer');
-        */
-
-        this.setState({loaded: !!res, data: res, parsedWeather: parsedData, city: res.name + ", " + res.sys.country});
+    componentDidMount() {
+        this.props.getLocation();
     }
 
 
@@ -35,23 +32,23 @@ class CurrentCity extends React.Component {
         <div className="current_city">
             {
                 this.state.loaded ?
-                <div className="weather_frame_now">
-                    <div className="city_name">{this.state.city}</div>
-                    <div className="date">{new Date().toDateString()}</div>
-                    <div className="weather_now">
-                        <div className="temperature_c">
-                            <span>{(this.state.data.main.temp - 273.14).toFixed(1) + "°C"}</span>
+                    <div className="weather_frame_now">
+                        <div className="city_name">{this.state.city}</div>
+                        <div className="date">{new Date().toDateString()}</div>
+                        <div className="weather_now">
+                            <div className="temperature_c">
+                                <span>{(this.state.data.main.temp - 273.14).toFixed(1) + "°C"}</span>
+                            </div>
+                            <img className="weather_icon" src={"http://openweathermap.org/img/wn/" +
+                            this.state.data.weather[0].icon + "@2x.png"} alt=""
+                            />
+                            <div className="weather_description">{this.state.data.weather[0].description}</div>
                         </div>
-                        <img className="weather_icon" src={"http://openweathermap.org/img/wn/" +
-                        this.state.data.weather[0].icon + "@2x.png"} alt=""
-                        />
-                        <div className="weather_description">{this.state.data.weather[0].description}</div>
                     </div>
-                </div>
                     :
                     <div className="image_container_overlay">
                         <p className="label">Подождите, данные загружаются</p>
-                        <CircularSpinner />
+                        <CircularSpinner/>
                     </div>
             }
             {
@@ -71,4 +68,12 @@ class CurrentCity extends React.Component {
     )
 }
 
-export default CurrentCity;
+const mapStateToProps = (state) => ({
+    geolocation: state.geolocation
+});
+
+const mapDispatchToProps = (dispatch) => ({
+    getLocation: () => dispatch(getGeolocation())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CurrentCity);
