a6e92329d4b6bf43ac0ce87847207a9b246bb8df
PiratBolot <remedictes@gmail.com>
2019-12-21 09:15:27 +0300

    Changed Store.js

--- src/store/Store.js
+++ src/store/Store.js
@@ -2,25 +2,16 @@ import {applyMiddleware, createStore} from 'redux'
 import rootReducer from "../reducers/Reducers";
 import thunk from "redux-thunk";
 
-const _loadState = () => JSON.parse(localStorage.state || "[]");
-const _storeState = (state) => localStorage.state = JSON.stringify(state);
-
-let persistentState = _loadState();
-
 let initialState = {
-    trackedCities: persistentState,
+    trackedCities: [],
     errorMessage: {isError: false, errorMsg: ""},
     geolocation: {}
 };
 
 export default function configureStore() {
-    let store = createStore(
+    return createStore(
         rootReducer,
         initialState,
         applyMiddleware(thunk)
     );
-    store.subscribe(() => {
-        _storeState(store.getState().trackedCities)
-    });
-    return store;
 };
