8788661841554686b4d2fff4810774f48b22290a
PiratBolot <remedictes@gmail.com>
2019-12-21 09:16:03 +0300

    Added componentWillMount in App

--- src/App.js
+++ src/App.js
@@ -6,12 +6,19 @@ import {Provider} from 'react-redux';
 import CurrentCity from './components/currentCity/CurrentCity'
 import TrackedCitiesPanel from "./components/trackedCitiesPanel/TrackedCitiesPanel";
 import configureStore from "./store/Store";
+import {fetchFavouriteCities, serverFetchFavouriteCities} from "./actions/Actions";
 import ErrorLine from "./components/errorLine/ErrorLine";
 import HeaderMenu from "./components/headerMenu/HeaderMenu";
 
 const store = configureStore();
 
 class App extends React.Component {
+
+    async componentWillMount() {
+        const res = await store.dispatch(serverFetchFavouriteCities);
+        store.dispatch(fetchFavouriteCities(res));
+    }
+
     render() {
         return (
             <div>
